c32fde3cdaaf6a00774cb5637805c013ff055036
kellary <51315509+kellary@users.noreply.github.com>
2022-06-10 20:11:45 +0300

    Add files via upload

---
+++ cpp.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include "windows.h"
+#include <iomanip>
+
+using namespace std;
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int cnt = 0;
+    int a, b, c;
+    cout << "\nВведіть A:";
+    cin >> a;
+    if (a > 0) cnt++;
+    cout << "\nВВедіть B:";
+    cin >> b;
+    if (b > 0) cnt++;
+    cout << "\nВВедыть C:";
+    cin >> c;
+    if (c > 0) cnt++;
+    if (cnt == 2) cout << "Позитивне"; else
+    {
+        cout << "Негавтивне";
+        system("pause");
+        return 0;
+    }
+}
